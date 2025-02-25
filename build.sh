d15ece09c120372171e405e052f78d642127dd45
Marcos Portela <marcosnasp@gmail.com>
2025-02-25 07:55:23 -0300

    conf: informe arquivo main.go para o build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p build
-GOOS=linux GOARCH=amd64 go build -o build/gocreate-linux-amd64 ./cmd/gocreate
-GOOS=windows GOARCH=amd64 go build -o build/gocreate-windows-amd64.exe ./cmd/gocreate
-GOOS=darwin GOARCH=amd64 go build -o build/gocreate-darwin-amd64 ./cmd/gocreate
+GOOS=linux GOARCH=amd64 go build -o build/gocreate-linux-amd64 ./cmd/gocreate-cli/main.go
+GOOS=windows GOARCH=amd64 go build -o build/gocreate-windows-amd64.exe ./cmd/gocreate-cli/main.go
+GOOS=darwin GOARCH=amd64 go build -o build/gocreate-darwin-amd64 ./cmd/gocreate-cli/main.go
 echo "Binários gerados em build/"
